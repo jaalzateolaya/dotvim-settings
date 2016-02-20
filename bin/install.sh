3100c288adbb8f8e948e3a8eda6db84ba62a97e6
814ckf0x <814ckf0x@gmail.com>
2016-02-20 17:19:51 -0500

    Add Vim Dir Install Support

--- bin/install.sh
+++ bin/install.sh
@@ -5,14 +5,18 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Install vundle
 vundle_dir=~/.vim/bundle/Vundle.vim
 
-if [ -d $vundle_dir ]; then
+if [ ! -d $vundle_dir ]; then
 	git clone https://github.com/VundleVim/Vundle.vim.git $vundle_dir
 fi
 
-# FIXME: CWD dependency MUST be fixed.
+# QUESTION: should we use install script instead cp?
 # Copy Vimrc to home directory
 cp {$DIR/../,~/.}vimrc
+
 # Copy Gvimrc to home directory
 cp {$DIR/../,~/.}gvimrc
+
+# Copy vim dir to home directory
+cp -r {$DIR/../,~/.}vim
 
 vim +BundleClean +BundleInstall! "+let g:session_autoload='no'" +qall
