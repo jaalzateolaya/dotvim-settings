6031f10f705fdfe2a935d41a7dc3f9d1cfbecbb7
Alexander Alzate Olaya <jhonnyalexander_91@yahoo.es>
2017-03-13 18:35:43 -0500

    Refactor installation script

--- bin/install.sh
+++ bin/install.sh
@@ -1,88 +1,84 @@
 #!/usr/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR=$( dirname "${BASH_SOURCE[0]}" )/..
 
-install_plugin=1 # Install vundle
-run_plugin_installer=1 # Perform plugins installation
-run_plugin_cleaner=1 # Run plugin cleaner before install
-quit_at_finished=1 # Quit when finished
+install_plugin_manager=1
+install_plugins=1
+clean_unused_plugins=1
+quit_when_finished=1
 
 help_msg () {
 	echo "Usage:"
-	echo "	$0 [-v] [-b] [-c] [-q]"
+	echo "	$0 [-c] [-i] [-p] [-q] [-h]"
 	echo "Options:"
-	echo "	-v Do not install (V)undle plugin."
-	echo "	-b Do not run (B)undleInstall! into vim."
-	echo "	-c Do not run Bundle(C)lean into vim."
-	echo "	-q Do not (q)uit vim when finished."
+	echo "	-c Do not automatically clean unused plugins."
+	echo "	-i Do not install plugin manager."
+	echo "	-p Do not automatically install plugins."
+	echo "	-q Do not quit vim once finished."
 	echo "	-h Show this help and exit."
 	echo
 	exit 1
 }
 
+announce () {
+	echo $(date +%c): ${1}...
+}
+
 while getopts :vbcqh opt
 do
 	case $opt in
-		v ) install_plugin=0
-			;;
-		b ) run_plugin_installer=0
-			;;
-		c ) run_plugin_cleaner=0
-			;;
-		q ) quit_at_finished=0
-			;;
-		h ) help_msg
-			;;
-		\?) help_msg
-			;;
+		v ) install_plugin_manager=0 ;;
+		b ) install_plugins=0 ;;
+		c ) clean_unused_plugins=0 ;;
+		q ) quit_when_finished=0 ;;
+		\?) help_msg ;;
 	esac
 done
 
-echo "Installing..."
-
-# Install vundle
-if [ 1 -eq $install_plugin ]; then
-	vundle_dir=~/.vim/bundle/Vundle.vim
+announce "Installing plugin manager"
+if [ 1 -eq $install_plugin_manager ]; then
+	vundle_dir=$HOME/.vim/bundle/Vundle.vim
 
 	if [ ! -d $vundle_dir ]; then
 		git clone https://github.com/VundleVim/Vundle.vim.git $vundle_dir
 	fi
 fi
 
-# QUESTION: should we use install script instead cp?
-# Copy Vimrc to home directory
-cp {$DIR/../,~/.}vimrc
+announce "Installing vim initiallization file (vimrc)"
+install -m 0600 {$DIR/,$HOME/.}vimrc
 
-# Copy Gvimrc to home directory
-cp {$DIR/../,~/.}gvimrc
+announce "Installing gvim initialization file (gvimrc)"
+install -m 0600 {$DIR/,$HOME/.}gvimrc
 
 # Copy vim dir to home directory
 if [ ! -d ~/.vim ]; then
 	mkdir ~/.vim
 fi
 
-cp -r $DIR/../vim/* ~/.vim
+announce "Installing vim folder"
+cp -r $DIR/vim/* $HOME/.vim
 
 # Vim commands to be executed.
 commands=( +BundleClean +BundleInstall! +qall )
 
-if [ 0 -eq $run_plugin_cleaner ]; then
+if [ 0 -eq $clean_unused_plugins ]; then
 	unset commands[0]
 fi
 
-if [ 0 -eq $run_plugin_installer ]; then
+if [ 0 -eq $install_plugins ]; then
 	unset commands[1]
 fi
 
 # If there is 1 element in the array, at this point, it must be +qall, so, don't
 # run vim.
 if [ ! 1 -eq ${#commands[@]} ]; then
-	if [ 0 -eq $quit_at_finished ]; then
+	if [ 0 -eq $quit_when_finished ]; then
 		unset commands[2]
 	fi
 
+    announce "Launching vim"
 	vim "+let g:session_autoload='no'" ${commands[@]}
 fi
 
-echo "Finished!"
+announce "Finished!"
 
